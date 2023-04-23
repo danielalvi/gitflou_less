fdb730706c06d25b2795e51bfe52ae9a06c1649c
simps <dc9204110@gmai.com>
2023-04-23 17:30:33 +0300

    added new function title

--- str_func.py
+++ str_func.py
@@ -6,5 +6,19 @@ def upper_string():
     up_string = my_string.upper()
     return up_string
 
-print(upper_string())
-print(upper_string.__doc__)
+
+
+def title_string():
+    """ Функция получает на ввод строку
+    и выводит первое слово
+    с заглавной буквы"""
+    my_title_string = str(input())
+    title_string = my_title_string.title()
+    return title_string
+
+if __name__ == '__main__':
+    print(upper_string())
+    print(upper_string.__doc__)
+    print(title_string())
+    print(title_string.__doc__)
+
