8dd2dc5882cb21a4c8a7c9adcbdb9a2b4767549a
danielalvi <131279637+danielalvi@users.noreply.github.com>
2023-04-23 10:15:15 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,10 @@
 def upper_string():
+    """Функция получает строку и выводит ее 
+    заглавными буквами
+    """
     my_string = str(input())
     up_string = my_string.upper()
     return up_string
 
 print(upper_string())
-
+print(upper_string.__doc__)
